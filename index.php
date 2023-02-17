206793a0ea2af5f880d1f841f10b4bcb5e5e1a77
Viraj Warhade <86669937+virajwarhade@users.noreply.github.com>
2023-02-17 18:34:59 +0530

    #3 commit

--- index.php
+++ index.php
@@ -29,14 +29,7 @@ while($row = mysqli_fetch_array($result)){
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="autocomplete.js"></script>
-    
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
 
 
@@ -47,80 +40,73 @@ while($row = mysqli_fetch_array($result)){
 <form method="post" >
 
 
+
+
 <div class="container-sm col-sm-6 col-12 mt-5 row mx-auto">
 
 
     <div class="mt-5 shadow rounded-3 bg-white border">
-
-
-
-       <!-- <div class="mb-3">
-          <label for="nameInput" class="form-label">Name</label>
-          <input type="text"class="form-control" name="name" id="nameInput" aria-describedby="helpId" placeholder="">
-        </div>
--->
-  
       <div class="mb-3 mt-4">     
-      <div class="dropdown">
-        <input type="text" class="form-control" id="myInput" placeholder="Type Name">
-
-            <div class="dropdown-menu" aria-labelledby="myInput" id="myDropdown">
-
+        <div class="dropdown">
+          <input type="text" class="form-control" id="myInput" placeholder="Type Name">
+          <div class="dropdown-menu" aria-labelledby="myInput" id="myDropdown"></div>
         </div>
       </div>
+
+
+      <div class="mb-3">
+        <label for="phno" class="form-label">Phone Number</label>
+        <input type="number" class="form-control" name="phone" id="phno" aria-describedby="helpId" placeholder="">
       </div>
- 
-<script>
-
-// Attach event listener to input field
-$("#myInput").on("input", function() {
-  // Retrieve value from input field
-  var value = $(this).val().toLowerCase();
-
-  // Filter the names array to find matching values
-  var matches = names.filter(function(name) {
-    return name.toLowerCase().startsWith(value);
-  });
-
-  // Build the dropdown menu
-  var dropdownMenu = $("#myDropdown");
-  dropdownMenu.empty();
-  for (var i = 0; i < matches.length; i++) {
-    var dropdownItem = $("<a class='dropdown-item'></a>");
-    dropdownItem.text(matches[i]);
-    dropdownItem.on("click", function() {
-      $("#myInput").val($(this).text());
-    });
-    dropdownMenu.append(dropdownItem);
-  }
-
-  // Show or hide the dropdown menu as needed
-  if (matches.length > 0) {
-    dropdownMenu.show();
-  } else {
-    dropdownMenu.hide();
-  }
-});
-
-
-  </script>
-
-
-
-
-        <div class="mb-3">
-          <label for="phno" class="form-label">Phone Number</label>
-          <input type="number"
-            class="form-control" name="phone" id="phno" aria-describedby="helpId" placeholder="">
-          
-        </div>
-
-
-   
     </div>
 
 
-</div>
+  </div>
+
+
+
+
+<!-- Add necessary JavaScript and jQuery libraries here -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+  <script>
+    // Define the array of names
+    var names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Harry", "Isabelle", "John"];
+
+    // Attach event listener to input field
+    $("#myInput").on("input", function() {
+      // Retrieve value from input field
+      var value = $(this).val().toLowerCase();
+
+      // Filter the names array to find matching values
+      var matches = names.filter(function(name) {
+        return name.toLowerCase().startsWith(value);
+      });
+
+      // Build the dropdown menu
+      var dropdownMenu = $("#myDropdown");
+      dropdownMenu.empty();
+      for (var i = 0; i < matches.length; i++) {
+        var dropdownItem = $("<a class='dropdown-item'></a>");
+        dropdownItem.text(matches[i]);
+        dropdownItem.on("click", function() {
+          $("#myInput").val($(this).text());
+        });
+        dropdownMenu.append(dropdownItem);
+      }
+
+      // Show or hide the dropdown menu as needed
+      if (matches.length > 0) {
+        dropdownMenu.show();
+      } else {
+        dropdownMenu.hide();
+      }
+    });
+  </script>
+
+
 
 
 
